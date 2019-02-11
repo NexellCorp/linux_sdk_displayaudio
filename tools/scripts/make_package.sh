57f21cc2201979e27467113fb1d1264fa5a979c8
Daegeun-Han <daegeun.han@nexell.co.kr>
2019-02-11 17:11:14 +0900

    NxBTService: added configuration file to support various boards.
    Changed:
     -.Modified make_package.sh script file.
     -.Removed the alsa related content from the Makefile.
     -.Applied to the NXBT API based on the configuration file.
    
    Signed-off-by: Daegeun-Han <daegeun.han@nexell.co.kr>
    Change-Id: Id9374bf155364e8bbec22cde407da90d42dcedfb

--- tools/scripts/make_package.sh
+++ tools/scripts/make_package.sh
@@ -93,6 +93,11 @@ function package_bt_service_application()
 	mkdir -p ${RESULT_DIR}/${QT_APP_ROOT}/${app_name}
 	cp -apvR ${TOP}/apps/${app_name}/*.so ${RESULT_DIR}/${QT_APP_ROOT}/${app_name}/
 	cp -apvR ${TOP}/apps/${app_name}/${PACKAGE_DIR}/* ${RESULT_DIR}/${QT_APP_ROOT}/${app_name}/
+	if [ ${TARGET_MACHINE} == "s5p4418-daudio-ref" ]; then
+		cp -apvR ${RESULT_DIR}/${QT_APP_ROOT}/${app_name}/nxbtservice_config_daudio_ref.xml ${RESULT_DIR}/${QT_APP_ROOT}/${app_name}/nxbtservice_config.xml
+	else
+		cp -apvR ${RESULT_DIR}/${QT_APP_ROOT}/${app_name}/nxbtservice_config_convergence.xml ${RESULT_DIR}/${QT_APP_ROOT}/${app_name}/nxbtservice_config.xml
+	fi
 }
 
 function unpackage_bt_service_application()
