8d7644695f6bc150fa74d2a7d5afe0f8cd2aab7c
HeynChul Jun <hcjun@nexell.co.kr>
2019-02-14 20:07:40 +0900

    VidopPlayer: add video speed control
    1.This patch adjusted the tab size.
    2.This patch solves the problem of not getting the previous location
      when running the video player while running bt audio.
      2.1. Lost audio focus in the video player and saved current position.
    3.This patch fixes an issue with opening audio devices
      when switching audio focus.
      3.1. When switching the audio focus, PlaySeek() was called
       after the initialization(mainframe) was over.
    4.This patch adds video speed control.
    5.This patch added libplayer to video speed support certain files.
      (container: avi,mp4,mkv  codec type:h264, mpeg4)
    
    Signed-off-by: HeynChul Jun <hcjun@nexell.co.kr>
    Change-Id: Idada49447a10aaa677854261ea2d043b7be06584
    Reviewed-on: https://review.gerrithub.io/c/444566
    Reviewed-by: SeongO Park <ray@nexell.co.kr>
    Tested-by: SeongO Park <ray@nexell.co.kr>

--- apps/NxVideoPlayer/CNX_Util.h
+++ apps/NxVideoPlayer/CNX_Util.h
@@ -11,6 +11,9 @@
 #include <unistd.h>
 #include <sys/time.h>
 
+#define		NX_ESEM					-1
+#define		NX_ESEM_OVERFLOW		-2					//	Exceed Max Value
+
 
 ////////////////////////////////////////////////////////////////////////////////
 //
@@ -18,63 +21,63 @@
 //
 class CNX_AutoLock {
 public:
-    CNX_AutoLock( pthread_mutex_t *pLock )
-        : m_pLock(pLock)
-    {
-        pthread_mutex_lock( m_pLock );
-    }
-    ~CNX_AutoLock()
-    {
-        pthread_mutex_unlock( m_pLock );
-    }
+	CNX_AutoLock( pthread_mutex_t *pLock )
+		: m_pLock(pLock)
+	{
+		pthread_mutex_lock( m_pLock );
+	}
+	~CNX_AutoLock()
+	{
+		pthread_mutex_unlock( m_pLock );
+	}
 
 protected:
-    pthread_mutex_t *m_pLock;
+	pthread_mutex_t *m_pLock;
 
 private:
-    CNX_AutoLock (const CNX_AutoLock &Ref);
-    CNX_AutoLock &operator=(CNX_AutoLock &Ref);
+	CNX_AutoLock (const CNX_AutoLock &Ref);
+	CNX_AutoLock &operator=(CNX_AutoLock &Ref);
 };
 
 
 class NX_CMutex
 {
 public:
-    NX_CMutex()
-    {
-        pthread_mutex_init( &m_hMutex, NULL );
-    }
+	NX_CMutex()
+	{
+		pthread_mutex_init( &m_hMutex, NULL );
+	}
 
-    ~NX_CMutex()
-    {
-        pthread_mutex_destroy( &m_hMutex );
-    }
+	~NX_CMutex()
+	{
+		pthread_mutex_destroy( &m_hMutex );
+	}
 
 public:
-    void Lock()
-    {
-        pthread_mutex_lock( &m_hMutex );
-    }
+	void Lock()
+	{
+		pthread_mutex_lock( &m_hMutex );
+	}
 
-    void Unlock()
-    {
-        pthread_mutex_unlock( &m_hMutex );
-    }
-
-private:
-    pthread_mutex_t		m_hMutex;
+	void Unlock()
+	{
+		pthread_mutex_unlock( &m_hMutex );
+	}
 
 private:
-    NX_CMutex (const NX_CMutex &Ref);
-    NX_CMutex &operator=(const NX_CMutex &Ref);
+	pthread_mutex_t		m_hMutex;
+
+private:
+	NX_CMutex (const NX_CMutex &Ref);
+	NX_CMutex &operator=(const NX_CMutex &Ref);
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 
 struct _NX_VDEC_SEMAPHORE{
-    uint				value;
-    pthread_cond_t		cond;
-    pthread_mutex_t		mutex;
+	uint				value;
+	pthread_cond_t		cond;
+	pthread_mutex_t		mutex;
 };
 typedef struct _NX_VDEC_SEMAPHORE NX_VDEC_SEMAPHORE;
 
@@ -90,124 +93,119 @@ int VDecSemSignal( NX_VDEC_SEMAPHORE *pSem );
 //
 class NX_CSemaphore{
 public:
-    NX_CSemaphore()
-        : m_iValue	( 1 )
-        , m_iMax	( 1 )
-        , m_iInit	( 1 )
-        , m_bReset	( false )
-        , m_iTot    ( 0 )
-    {
-        pthread_cond_init ( &m_hCond,  NULL );
-        pthread_mutex_init( &m_hMutex, NULL );
-    }
+	NX_CSemaphore()
+		: m_iValue	( 0 )
+		, m_iMax	( 1 )
+		, m_iInit	( 0 )
+		, m_bReset	( false )
+		, m_iTot    ( 0 )
+	{
+		pthread_cond_init ( &m_hCond,  NULL );
+		pthread_mutex_init( &m_hMutex, NULL );
+	}
 
-    NX_CSemaphore( int32_t iMax, int32_t iInit )
-        : m_iValue	( iInit )
-        , m_iMax	( iMax )
-        , m_iInit	( iInit )
-        , m_bReset	( false )
-    {
-        pthread_cond_init ( &m_hCond,  NULL );
-        pthread_mutex_init( &m_hMutex, NULL );
-    }
+	NX_CSemaphore( int32_t iMax, int32_t iInit )
+		: m_iValue	( iInit )
+		, m_iMax	( iMax )
+		, m_iInit	( iInit )
+		, m_bReset	( false )
+	{
+		pthread_cond_init ( &m_hCond,  NULL );
+		pthread_mutex_init( &m_hMutex, NULL );
+	}
 
-    ~NX_CSemaphore()
-    {
-        ResetSignal();
-        pthread_cond_destroy( &m_hCond );
-        pthread_mutex_destroy( &m_hMutex );
-    }
+	~NX_CSemaphore()
+	{
+		ResetSignal();
+		pthread_cond_destroy( &m_hCond );
+		pthread_mutex_destroy( &m_hMutex );
+	}
 
 public:
-    int32_t Post()
-    {
-        int32_t iRet = 0;
-        pthread_mutex_lock( &m_hMutex );
-        m_iValue ++;
-        pthread_cond_signal( &m_hCond );
-        pthread_mutex_unlock( &m_hMutex );
-        return iRet;
-    }
+	int32_t Post()
+	{
+		int32_t iRet = 0;
 
-    int32_t Pend()
-    {
-        int32_t iRet = 0;
-        pthread_mutex_lock( &m_hMutex );
+		pthread_mutex_lock( &m_hMutex );
+		if(m_iValue >= m_iMax)
+		{
+			iRet = NX_ESEM_OVERFLOW;
+		}
+		else
+		{
+			m_iValue ++;
+		}
+		pthread_cond_signal( &m_hCond );
+		pthread_mutex_unlock( &m_hMutex );
 
-        m_iValue --;
+		return iRet;
+	}
 
-        if( m_iValue == 0 )
-        {
-            int ret = gettimeofday(&m_CurTime,NULL);
-            if(0 > ret) printf("gettimeofday err\n");
+	int32_t Pend()
+	{
+		int32_t iRet = 0;
+		pthread_mutex_lock( &m_hMutex );
 
-            m_Timeout.tv_sec = m_CurTime.tv_sec +1;
-            m_Timeout.tv_nsec = m_CurTime.tv_usec * 1000;
+		if( m_iValue == 0 )
+		{
+			iRet = pthread_cond_wait(&m_hCond,&m_hMutex);
 
-            int iResult = pthread_cond_timedwait(&m_hCond,&m_hMutex,&m_Timeout);
+			if( 0 != iRet )
+			{
+				iRet = NX_ESEM;
+			}
+			else
+			{
+				m_iValue--;
+			}
+		}
+		pthread_mutex_unlock( &m_hMutex );
+		return iRet;
+	}
 
-            switch (iResult) {
-            case 0:
-                //printf("signal ok\n");
-                break;
-            case ETIMEDOUT:
-                printf("\n\n***************signal not ok,, calling this->update(); for paintGL is not worked!!\n\n");
-                break;
-            default:
-                printf("pthread cond timedwait err\n");
-                break;
-            }
-        }
-        pthread_mutex_unlock( &m_hMutex );
-        return iRet;
-    }
+	void ResetSignal()
+	{
+		pthread_mutex_lock ( &m_hMutex );
+		m_bReset = true;
 
-    void ResetSignal()
-    {
-        pthread_mutex_lock ( &m_hMutex );
-        m_bReset = true;
+		for (int32_t i = 0; i<m_iMax; i++)
+		{
+			pthread_cond_signal( &m_hCond );
+		}
 
-        for (int32_t i = 0; i<m_iMax; i++)
-        {
-            pthread_cond_signal( &m_hCond );
-        }
+		m_iTot = 0;
+		pthread_mutex_unlock( &m_hMutex );
+	}
 
-        m_iTot = 0;
-        pthread_mutex_unlock( &m_hMutex );
-    }
+	void ResetValue()
+	{
+		pthread_mutex_lock( &m_hMutex );
+		m_iValue = m_iInit;
+		m_bReset = false;
+		pthread_mutex_unlock( &m_hMutex );
+	}
 
-    void ResetValue()
-    {
-        pthread_mutex_lock( &m_hMutex );
-        m_iValue = m_iInit;
-        m_bReset = false;
-        pthread_mutex_unlock( &m_hMutex );
-    }
-
-    int32_t GetValue()
-    {
-        pthread_mutex_lock( &m_hMutex );
-        int32_t iValue = m_iValue;
-        pthread_mutex_unlock( &m_hMutex );
-        return iValue;
-    }
+	int32_t GetValue()
+	{
+		pthread_mutex_lock( &m_hMutex );
+		int32_t iValue = m_iValue;
+		pthread_mutex_unlock( &m_hMutex );
+		return iValue;
+	}
 
 private:
-    enum { MAX_SEM_VALUE = 1024 };
+	enum { MAX_SEM_VALUE = 1024 };
 
-    pthread_cond_t  m_hCond;
-    pthread_mutex_t m_hMutex;
-    int32_t			m_iValue;
-    int32_t			m_iMax;
-    int32_t			m_iInit;
-    int32_t			m_bReset;
-    int32_t			m_iTot;
-
-    struct timeval m_CurTime;
-    struct timespec m_Timeout;
+	pthread_cond_t  m_hCond;
+	pthread_mutex_t m_hMutex;
+	int32_t			m_iValue;
+	int32_t			m_iMax;
+	int32_t			m_iInit;
+	int32_t			m_bReset;
+	int32_t			m_iTot;
 private:
-    NX_CSemaphore (const NX_CSemaphore &Ref);
-    NX_CSemaphore &operator=(const NX_CSemaphore &Ref);
+	NX_CSemaphore (const NX_CSemaphore &Ref);
+	NX_CSemaphore &operator=(const NX_CSemaphore &Ref);
 };
 
 
@@ -218,28 +216,28 @@ private:
 class NX_GetTickCount
 {
 public:
-    NX_GetTickCount()
-    {
-    }
+	NX_GetTickCount()
+	{
+	}
 
-    ~NX_GetTickCount()
-    {
-    }
+	~NX_GetTickCount()
+	{
+	}
 
 public:
-    int64_t GetTime()   //ms
-    {
-        int64_t Ret;
-        struct timeval	tv;
-        struct timezone	zv;
-        gettimeofday( &tv, &zv );
-        Ret = ((int64_t)tv.tv_sec)*1000 + (int64_t)(tv.tv_usec/1000);
-        return Ret;
-    }
+	int64_t GetTime()   //ms
+	{
+		int64_t Ret;
+		struct timeval	tv;
+		struct timezone	zv;
+		gettimeofday( &tv, &zv );
+		Ret = ((int64_t)tv.tv_sec)*1000 + (int64_t)(tv.tv_usec/1000);
+		return Ret;
+	}
 
 private:
-    NX_GetTickCount (const NX_GetTickCount &Ref);
-    NX_GetTickCount &operator=(const NX_GetTickCount &Ref);
+	NX_GetTickCount (const NX_GetTickCount &Ref);
+	NX_GetTickCount &operator=(const NX_GetTickCount &Ref);
 };
 
 #endif // CNX_Util_H
--- apps/NxVideoPlayer/PlayerVideoFrame.h
+++ apps/NxVideoPlayer/PlayerVideoFrame.h
@@ -20,6 +20,10 @@
 #include <QEvent>
 #include <NX_Type.h>
 
+//Message
+#include <QLabel>
+#include <QPushButton>
+
 //	xml config
 #include "NX_IConfig.h"
 //	scanner
@@ -30,19 +34,19 @@
 #include <QObject>
 class CallBackSignal : public QObject
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    CallBackSignal() {}
+	CallBackSignal() {}
 
 public slots:
-    void statusChanged(int eventType)
-    {
-        emit mediaStatusChanged(eventType);
-    }
+	void statusChanged(int eventType)
+	{
+		emit mediaStatusChanged(eventType);
+	}
 
 signals:
-    void mediaStatusChanged(int newValue);
+	void mediaStatusChanged(int newValue);
 };
 
 namespace Ui {
@@ -51,131 +55,144 @@ class PlayerVideoFrame;
 
 class PlayerVideoFrame : public QFrame
 {
-    Q_OBJECT
+	Q_OBJECT
 
 signals:
-    void signalPlayList();
+	void signalPlayList();
 
 private slots:
-    void slotPlayListFrameAccept();
-    void slotPlayListFrameReject();
-    void subTitleDisplayUpdate();
-    void statusChanged(int eventType);
-    void DoPositionUpdate();
+	void slotPlayListFrameAccept();
+	void slotPlayListFrameReject();
+	void subTitleDisplayUpdate();
+	void statusChanged(int eventType);
+	void DoPositionUpdate();
 
-    void on_prevButton_released();
-    void on_playButton_released();
-    void on_pauseButton_released();
-    void on_nextButton_released();
-    void on_stopButton_released();
-    void on_playListButton_released();
+	void on_prevButton_released();
+	void on_playButton_released();
+	void on_pauseButton_released();
+	void on_nextButton_released();
+	void on_stopButton_released();
+	void on_playListButton_released();
+
+	void on_speedButton_released();
+
+	void slotOk();
 
 public:
-    explicit PlayerVideoFrame(QWidget *parent = 0);
-    ~PlayerVideoFrame();
+	explicit PlayerVideoFrame(QWidget *parent = 0);
+	~PlayerVideoFrame();
 
 protected:
-    bool event(QEvent *e);
-    void resizeEvent(QResizeEvent *event);
+	bool event(QEvent *e);
+	void resizeEvent(QResizeEvent *event);
 
 private:
-    void TerminateEvent(NxTerminateEvent *e);
-    void StatusHomeEvent(NxStatusHomeEvent *e);
-    void StatusBackEvent(NxStatusBackEvent *e);
-    void StatusVolumeEvent(NxStatusVolumeEvent *e);
+	void TerminateEvent(NxTerminateEvent *e);
+	void StatusHomeEvent(NxStatusHomeEvent *e);
+	void StatusBackEvent(NxStatusBackEvent *e);
+	void StatusVolumeEvent(NxStatusVolumeEvent *e);
 
 public:
-    void RegisterRequestTerminate(void (*cbFunc)(void));
-    void RegisterRequestVolume(void (*cbFunc)(void));
-    void RegisterRequestLauncherShow(void (*cbFunc)(bool *bOk));
-    void getAspectRatio(int srcWidth, int srcHeight,
-                        int scrWidth, int scrHeight,
-                        int *pWidth, int *pHeight);
-    void displayTouchEvent();
+	void RegisterRequestTerminate(void (*cbFunc)(void));
+	void RegisterRequestVolume(void (*cbFunc)(void));
+	void RegisterRequestLauncherShow(void (*cbFunc)(bool *bOk));
+	void getAspectRatio(int srcWidth, int srcHeight,
+						int scrWidth, int scrHeight,
+						int *pWidth, int *pHeight);
+	void displayTouchEvent();
 
-    //	xml (save previous state)
-    int SaveInfo();
-    bool SeekToPrev(int* iSavedPosition, int* iFileIdx);
+	//	xml (save previous state)
+	int SaveInfo();
+	bool SeekToPrev(int* iSavedPosition, int* iFileIdx);
 
-    //	Storage Event
-    void StorageRemoved();
-    void StorageScanDone();
-    void UpdateFileList();
-    CNX_FileList* GetFileList();
+	//	Storage Event
+	void StorageRemoved();
+	void StorageScanDone();
+	void UpdateFileList();
+	CNX_FileList* GetFileList();
 
-    //
-    //	MediaPlayer
-    bool PauseVideo();
-    bool StopVideo();
-    bool PlayVideo();
-    bool CloseVideo();
-    bool SeekVideo( int32_t iSec );
-    bool SetVideoVolume( int32_t iVolume);
-    bool PlayNextVideo();
-    bool PlayPreviousVideo();
-    bool SetVideoMute(bool iVideoMute);
-    bool GetVideoMuteStatus();
-    bool VideoMute();
-    bool VideoMuteStart();
-    bool VideoMuteStop();
-    void PlaySeek();
+	//
+	//	MediaPlayer
+	bool PauseVideo();
+	bool StopVideo();
+	bool PlayVideo();
+	bool CloseVideo();
+	bool SeekVideo( int32_t iSec );
+	bool SetVideoVolume( int32_t iVolume);
+	bool PlayNextVideo();
+	bool PlayPreviousVideo();
+	bool SetVideoMute(bool iVideoMute);
+	bool GetVideoMuteStatus();
+	bool VideoMute();
+	bool VideoMuteStart();
+	bool VideoMuteStop();
+	void PlaySeek();
 
-    //
-    //	SubTitle
-    int	 OpenSubTitle();
-    void PlaySubTitle();
-    void StopSubTitle();
-
-private:
-    CNX_MoviePlayer	*m_pNxPlayer;
-    QTextCodec*		m_pCodec;
-    bool			m_bSubThreadFlag;
-    int				m_iVolValue;
-    qint64			m_iDuration;
-    QTimer			m_PosUpdateTimer;
-    bool			m_bIsInitialized;
-    NX_CMutex		m_statusMutex;
-    //	UI Status Bar
-    CNX_StatusBar* m_pStatusBar;
-
-    //	Progress Bar
-    bool	m_bSeekReady;
-    bool	m_bButtonHide;
-
-    //	event filter
-    bool eventFilter(QObject *watched, QEvent *event);
+	//
+	//	SubTitle
+	int	 OpenSubTitle();
+	void PlaySubTitle();
+	void StopSubTitle();
 
 private:
-    void SetupUI();
-    void UpdateDurationInfo(int64_t position, int64_t duration);
+	CNX_MoviePlayer	*m_pNxPlayer;
+	QTextCodec*		m_pCodec;
+	bool			m_bSubThreadFlag;
+	int				m_iVolValue;
+	qint64			m_iDuration;
+	QTimer			m_PosUpdateTimer;
+	bool			m_bIsInitialized;
+	NX_CMutex		m_statusMutex;
+	//	UI Status Bar
+	CNX_StatusBar* m_pStatusBar;
 
-    //  Update Progress Bar
+	//	Progress Bar
+	bool	m_bSeekReady;
+	bool	m_bButtonHide;
+
+	//	event filter
+	bool eventFilter(QObject *watched, QEvent *event);
+
 private:
-    void updateProgressBar(QMouseEvent *event, bool bReleased);
+	void SetupUI();
+	void UpdateDurationInfo(int64_t position, int64_t duration);
+
+	//  Update Progress Bar
+private:
+	void updateProgressBar(QMouseEvent *event, bool bReleased);
 
 public:
-    void setVideoFocus(bool bVideoFocus);
-    void setAudioFocus(bool bAudioFocus);
+	void setVideoFocus(bool bVideoFocus);
+	void setAudioFocus(bool bAudioFocus);
 
 private:
-    int		m_iCurFileListIdx;	//index of media list
-    CNX_FileList	m_FileList;	//media list
-    NX_IConfig*		m_pIConfig;	//xml
-    bool	m_bTurnOffFlag;		//for close control
-    bool	m_bStopRenderingFlag;//flag for stopping rendering callback
-    bool	m_bTryFlag;			//try playing back last status
-    NX_CMutex	m_listMutex;	//for storage event
-    PlayListVideoFrame* m_pPlayListFrame;
-    bool 	m_bIsVideoFocus;
-    bool 	m_bIsAudioFocus;
+	int		m_iCurFileListIdx;	//index of media list
+	CNX_FileList	m_FileList;	//media list
+	NX_IConfig*		m_pIConfig;	//xml
+	bool	m_bTurnOffFlag;		//for close control
+	bool	m_bStopRenderingFlag;//flag for stopping rendering callback
+	bool	m_bTryFlag;			//try playing back last status
+	NX_CMutex	m_listMutex;	//for storage event
+	PlayListVideoFrame* m_pPlayListFrame;
+	bool 	m_bIsVideoFocus;
+	bool 	m_bIsAudioFocus;
 
-    // Terminate
-    void (*m_pRequestTerminate)(void);
-    void (*m_pRequestLauncherShow)(bool *bOk);
-    void (*m_pRequestVolume)(void);
+	// Terminate
+	void (*m_pRequestTerminate)(void);
+	void (*m_pRequestLauncherShow)(bool *bOk);
+	void (*m_pRequestVolume)(void);
+
+	// Video Speed
+	float m_fSpeed;
+	bool m_bNotSupportSpeed;
+
+	//Message
+	QFrame *m_pMessageFrame;
+	QLabel *m_pMessageLabel;
+	QPushButton *m_pMessageButton;
 
 private:
-    Ui::PlayerVideoFrame *ui;
+	Ui::PlayerVideoFrame *ui;
 };
 
 #endif // PLAYERVIDEOFRAME_H
