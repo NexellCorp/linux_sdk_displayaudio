5a26b6b7b7e084f46f385d5ae28f501728df04e9
Daegeun-Han <daegeun.han@nexell.co.kr>
2019-02-28 14:47:06 +0900

    NxBTService: added NxUtils source/header files for utility functions.
    Changed:
     -.added NxUtils.cpp and NxUtils.h
      -.utility functions in the NxBTService source have been migrated to
      NxUtils.
      -.Rename utility functions.
       -.The first character starts with an uppercase letter.
    
    Signed-off-by: Daegeun-Han <daegeun.han@nexell.co.kr>
    Change-Id: Idff5de96a45b1147f7e79a87ce6b7672f5981ddb

---
+++ apps/NxBTService/NxUtils.cpp
@@ -0,0 +1,157 @@
+#include "NxUtils.h"
+#include <locale>
+#include <string.h>
+
+vector<string> CreateTokensFromCommand(const char* command)
+{
+	string target = (string)command;
+	vector<string> tokens;
+	string token;
+	int stx, etx;
+
+	stx = target.find("$");
+	if (stx < 0) {
+		goto error_occur;
+	}
+
+	etx = target.find("\n", stx);
+	if (etx < 0) {
+		goto error_occur;
+	}
+
+	for (int i = stx+1; i < etx; i++) {
+		if (target[i] == '#') {
+			tokens.push_back(token);
+			token.clear();
+			continue;
+		}
+
+		token += target[i];
+	}
+
+	if (token.length()) {
+		tokens.push_back(token);
+	}
+
+error_occur:
+	return tokens;
+}
+
+vector<string> CreateTokens(string text, char seperator, char stx/*= 0*/, char etx/*= 0*/)
+{
+	vector<string> tokens;
+	string token;
+	int start = 0, end = text.length();
+
+	if (stx) {
+		start = text.find(stx);
+
+		if (start < 0) {
+			goto loop_finish;
+		}
+	}
+
+	if (etx) {
+		end = text.find(etx, stx);
+
+		if (end < 0) {
+			goto loop_finish;
+		}
+	}
+
+	for (int i = start; i < end; i++) {
+		if (text[i] == seperator) {
+			tokens.push_back(token);
+			token.clear();
+			continue;
+		}
+
+		token += text[i];
+	}
+
+	if (token.length()) {
+		tokens.push_back(token);
+	}
+
+loop_finish:
+	return tokens;
+}
+
+string FindArgument(std::string* command)
+{
+	size_t pos = command->rfind(" ");
+	if (pos == string::npos) {
+		return string();
+	}
+	++pos;
+
+	return command->substr(pos, command->length()-pos);
+}
+
+bool FindArgument(string* command, string target, string* argument)
+{
+	argument->clear();
+
+	for (size_t i = strlen(target.c_str()); i < command->length(); i++) {
+		*argument += command->at(i);
+	}
+
+	return !argument->empty();
+}
+
+string MakeReplyCommand(bool ok, vector<string> reply)
+{
+	std::string command;
+
+	command += COMMAND_FORMAT_STX;
+	command += ok ? COMMAND_FORMAT_REPLY_DONE : COMMAND_FORMAT_REPLY_FAIL;
+
+	for (size_t i = 0; i < reply.size(); i++) {
+		command += COMMAND_FORMAT_SEPERATOR + reply[i];
+	}
+
+	command += COMMAND_FORMAT_ETX;
+
+	return command;
+}
+
+bool IsDigit(std::string text)
+{
+	if (text.empty()) {
+		return false;
+	}
+
+	for (size_t i = 0; i < text.length(); i++) {
+		if (text[i] < '0' || '9' < text[i]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+string ToLower(string str)
+{
+	locale l;
+	string ret;
+
+	for (size_t i = 0; i < str.length(); ++i)
+	{
+		ret += tolower(str[i], l);
+	}
+
+	return ret;
+}
+
+string ToUpper(string str)
+{
+	locale l;
+	string ret;
+
+	for (size_t i = 9; i < str.length(); ++i)
+	{
+		ret += toupper(str[i], l);
+	}
+
+	return ret;
+}
