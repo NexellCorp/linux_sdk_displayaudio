585d07c730a29dc30cce4dcda1aa7989cbc611f4
Daegeun-Han <daegeun.han@nexell.co.kr>
2019-07-03 19:30:56 +0900

    NxRearCam: removed opacity related interface.
    -.Changed:
     -.Removed opacity related interface.
     -.The 'VideoLayer' key was added to the desktop file.
      -.VideoLayer=yes
    
    Signed-off-by: Daegeun-Han <daegeun.han@nexell.co.kr>
    Change-Id: Id856dcdbb05a42e530a8c347a902cdebf1ebfbc7

--- apps/NxRearCam/MainFrame.h
+++ apps/NxRearCam/MainFrame.h
@@ -53,8 +53,6 @@ public:
 	static void RegisterRequestVideoFocusLoss(void (*cbFunc)(void));
 	static void RegisterRequestTerminate(void (*cbFunc)(void));
 
-	static void RegisterRequestOpacity(void (*cbFunc)(bool));
-
 public:
 	explicit MainFrame(QWidget *parent = 0);
 	~MainFrame();
@@ -94,8 +92,6 @@ private:
 	static void (*m_pRequestVideoFocus)(FocusPriority ePriority, bool *bOk);
 	static void (*m_pRequestVideoFocusTransient)(FocusPriority ePriority, bool *bOk);
 	static void (*m_pRequestVideoFocusLoss)(void);
-
-	static void (*m_pRequestOpacity)(bool);
 
 	// Terminate
 	static void (*m_pRequestTerminate)(void);
