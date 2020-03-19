b56522a7dbd5f18a3d152a71625d2380b29d7de1
Daegeun-Han <daegeun.han@nexell.co.kr>
2020-03-19 15:49:10 +0900

    push.sh: added handling for adb version compatibility.
    This patch adds handling for adb version compatibility.
    
    Signed-off-by: Daegeun-Han <daegeun.han@nexell.co.kr>
    Change-Id: I665e7a349a735401ce2b28ab2cc07fbdc67f764f

--- tools/scripts/push.sh
+++ tools/scripts/push.sh
@@ -4,8 +4,18 @@ echo ""
 echo "===== Displayaudio SDK pushing ====="
 echo ""
 RESULT_DIR="result"
-adb push ${RESULT_DIR} /
-adb shell sync
+if [ ! -d ${RESULT_DIR} ]; then
+	echo "result/ is no exist."
+	exit -1
+fi
+
+line=$(adb version)
+if [[ "$line" == *"android-sdk"* ]]; then
+	adb push ${RESULT_DIR}/* /
+else
+	adb push ${RESULT_DIR}/ /
+fi
+
 echo ""
 echo "===== Pushing complete ====="
 echo ""
